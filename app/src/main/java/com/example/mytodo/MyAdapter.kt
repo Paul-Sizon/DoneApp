462e6ed9c9c0a0804b5caa7439e79787074e0c1e
Paul-Sizon <pauldeal66@gmail.com>
2020-10-01 17:12:01 +0300

    diffutil

--- app/src/main/java/com/example/mytodo/MyAdapter.kt
+++ app/src/main/java/com/example/mytodo/MyAdapter.kt
@@ -4,12 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mytodo.data.Task
 import kotlinx.android.synthetic.main.items_layout.view.*
 
 
-class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+class MyAdapter : ListAdapter<Task, MyAdapter.MyViewHolder>(TaskDiffCallback()) {
     var myDataList = emptyList<Task>()
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -19,24 +21,29 @@ class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.items_layout, parent, false))
+        return MyViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.items_layout, parent, false)
+        )
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        val currentItem = myDataList[position]
+        val currentItem = getItem(position)
         holder.titleItem.text = currentItem.title
         holder.describItem.text = currentItem.describtion
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = myDataList.size
 
+    class TaskDiffCallback : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.taskId == newItem.taskId
+        }
 
-    fun setData(task: List<Task>) {
-        this.myDataList = task
-        notifyDataSetChanged()
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+
     }
 }
