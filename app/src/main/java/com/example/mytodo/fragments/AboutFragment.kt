e96c3974785668c87387224b01bba345ba9edc4f
Paul-Sizon <pauldeal66@gmail.com>
2020-10-14 14:34:28 +0300

    about fragment + email intent

---
+++ app/src/main/java/com/example/mytodo/fragments/AboutFragment.kt
@@ -0,0 +1,53 @@
+package com.example.mytodo.fragments
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.mytodo.R
+import kotlinx.android.synthetic.main.fragment_about.*
+
+class AboutFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+        view.findViewById<TextView>(R.id.email_adress).setOnClickListener { sendEmail() }
+
+
+
+
+
+
+        return view
+    }
+
+
+    private fun sendEmail() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.data = Uri.parse("mailto:paul.sizon@outlook.com") // only email apps should handle this
+        //intent.putExtra(Intent.EXTRA_EMAIL, "")
+        try {
+            //start email intent
+            startActivity(Intent.createChooser(intent, "Choose Email Client..."))
+           // startActivity(intent)
+        }
+        catch (e: Exception){
+            //if any thing goes wrong for example no email client application or any exception
+            //get and show exception message
+            Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+}
